d4eb79c0cf46d607024cbd423d82ae8da908113d
nitoyon <nitoyon@gmail.com>
2013-01-18 00:27:09 +0900

    Add test for WebSocketHandler

---
+++ test/test.websocket.js
@@ -0,0 +1,49 @@
+var should = require('should')
+  , ServerResponse = require('http').ServerResponse
+  , WebSocket = require('ws')
+  , WebSocketHandler = require('../lib/websocket');
+
+describe('WebSocketHandler', function() {
+  var handler, ws;
+
+  beforeEach(function() {
+    handler = new WebSocketHandler({}, { port: 8080 });
+    ws = new WebSocket('ws://localhost:8080');
+  });
+
+  afterEach(function() {
+    handler.close();
+    handler = ws = undefined;
+  });
+
+  it('should handle new connection', function(done) {
+    handler.clients.should.be.lengthOf(0);
+
+    ws.on('open', function() {
+      ws.send('{ "command": "hello" }');
+    });
+    ws.on('message', function(data, flags) {
+      var msg = JSON.parse(data);
+      msg.should.have.property('command', 'hello');
+      msg.protocols.should.be.an.instanceOf(Array);
+      msg.protocols.should.include('http://livereload.com/protocols/official-7');
+      handler.clients.should.be.lengthOf(1);
+      done();
+    });
+  });
+
+  it('should notify to clients', function(done) {
+    ws.on('open', function() {
+      ws.send('{ "command": "hello" }');
+    });
+    ws.on('message', function(data, flags) {
+      var msg = JSON.parse(data);
+      if (msg.command == 'hello') {
+        handler.send('/test.html', '{ "command": "reload", "path": "/test.html" }');
+      } else if (msg.command == 'reload') {
+        msg.path.should.equal('/test.html')
+        done();
+      }
+    });
+  });
+});

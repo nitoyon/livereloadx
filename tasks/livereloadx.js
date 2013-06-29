d61c398d44e997700050e35a495ee2d83d060127
nitoyon <nitoyon@gmail.com>
2013-06-29 23:00:52 +0900

    Add Grunt support
    Now, LiveReloadX module can be used as a grunt task.

---
+++ tasks/livereloadx.js
@@ -0,0 +1,37 @@
+/*
+ * livereloadx
+ * https://github.com/nitoyon/livereloadx
+ *
+ * Copyright (c) 2013 nitoyon
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('livereloadx', 'LiveReloadX server', function() {
+    // initialize log
+    var log_mod = require('../lib/log')
+      , config_mod = require('../lib/config');
+    log_mod.cli = true;
+    if (grunt.option('debug') || grunt.option('verbose')) {
+      log_mod.verbose = true;
+    }
+
+    // get config
+    var log = log_mod('grunt');
+    try {
+      var config = config_mod.setDefaultValue(grunt.config(this.name));
+      config_mod.dump(log.info.bind(log), config);
+    } catch (e) {
+      grunt.log.error(e);
+      return;
+    }
+
+    // start server
+    var server = require('../')(config);
+    server.listen().watch();
+    grunt.log.writeln('LiveReloadX started.');
+  });
+};
